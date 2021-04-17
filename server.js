96228f94815751ea34dd4ad44a0e59fffb808ad6
Shygois <shayana.gois@att.net>
2021-04-17 13:14:05 -0700

    added PORT environment variable

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 // parse incoming string or array data
@@ -17,6 +18,6 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
   });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
 });
